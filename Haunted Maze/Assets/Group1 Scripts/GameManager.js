3bb367276bbb4f72a6f4e53d82ad67e39ef58eed
ThompsonGSD <bthomp10@baker.edu>
2014-10-21 10:09:01 -0400

    Addition 10/20/14
    Tidbits added-mesh colliders to multiple items: both gates,
    jack-o-lantern, diamonds, ghost, flashlight, etc. Particles added to
    flashlight for effect. Inventory GUI added. Working on dialog GUI and
    Start Menu GUI.

---
+++ Haunted Maze/Assets/Group1 Scripts/GameManager.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+internal var iMode = false; //flag whether inventory mode is off or on
+
+function Start () {
+
+}
+
+function Update () {
+
+}
---
+++ Haunted Maze/Assets/Group1 Scripts/Rotate_Diamond_Script.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var myDegrees = 100;
+
+function Start () {
+
+}
+
+function Update () {
+
+transform.Rotate(myDegrees * Time.deltaTime, 0, 0);
+;
+}
