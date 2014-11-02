4fabf75387e67bc33ff968e4d38bae3d9da22025
ThompsonGSD <bthomp10@baker.edu>
2014-11-02 14:27:44 -0500

    Battery/PowerBar GUI and Player Texture for MiniMap
    I have created the battery texture and also a powerbar texture and
    scripted them into the Hud Script.
    The power bar will need instructions to Shrink its width over the time
    period that the flashlight remains active.
    The powerbar texture (originally white) should start as a nice green
    color and once it hits 75% of the original width to switch to a
    light green color. 50% of the original width will change the powerbar
    texture to Yellow and 25% of the original width to Red.
    I have placed a big white flat sphere shape over the playerobject so
    that it is very visible to the player in the minimap.
    As for the minimap, I struggled to get it to adjust to different screen
    resolution, but I have it attached to the game controller.
    I am working on the credit list right now... and will post it later or
    tomorrow morning depending on my fiance's schedule.

--- GameProject_Beta/Assets/Scripts/GameScripts/scr_HUD.cs
+++ GameProject_Beta/Assets/Scripts/GameScripts/scr_HUD.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class scr_HUD : MonoBehaviour
@@ -9,11 +9,14 @@ public class scr_HUD : MonoBehaviour
 	public GUIStyle guiStyle = new GUIStyle();
 	public Texture txtLives;
 	public Texture txtDiamonds;
+	public Texture txtPowerBar; //BT
+	public Texture txtBattery; //BT
+	public Camera miniMapCam; //BT
 
 	private scr_GameControl scrGameControl;
 	private int intNumOfLives;
 	private int intCollectibleCount;
-
+	
 	void Awake()
 	{
 		scrGameControl = GetComponent<scr_GameControl>();
@@ -29,7 +32,9 @@ public class scr_HUD : MonoBehaviour
 		intCollectibleCount = scrGameControl.GetCollectibleCount();
 
 		GUI.DrawTexture(new Rect(50f, 10f, 146f, 75f), txtLives);
-		GUI.DrawTexture(new Rect(Screen.width - 205f, 10f, 155f, 75f), txtDiamonds);
+		GUI.DrawTexture(new Rect(Screen.width - 205f, 10f, 155f, 75f), txtDiamonds); 
+		GUI.DrawTexture (new Rect (Screen.width - 158f, Screen.height - 62f, 100f, 30f), txtPowerBar); //BT
+		GUI.DrawTexture (new Rect (Screen.width - 160f, Screen.height - 65f, 120f, 40f), txtBattery); //BT
 		GUI.Label(new Rect(150f, 38f, 100f, 50f), intNumOfLives.ToString(), guiStyle);
 		GUI.Label(new Rect(Screen.width - 190f, 35f, 100f, 50f), intCollectibleCount.ToString() + " / 7", guiStyle);
 	}
